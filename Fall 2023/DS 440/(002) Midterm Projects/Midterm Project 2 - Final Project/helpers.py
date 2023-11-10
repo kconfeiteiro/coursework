0047946bb56093f28311c91d75db932f9426e2a9
Krystian Ojeda Confeiteiro <confeitk@outlook.com>
2023-11-10 17:40:16 -0500

    Started working on data -- need to split the large file into multiple smaller binned files (so I can push them to the remote repository).

--- Fall 2023/DS 440/(002) Midterm Projects/Midterm Project 2 - Final Project/helpers.py
+++ Fall 2023/DS 440/(002) Midterm Projects/Midterm Project 2 - Final Project/helpers.py
@@ -1,5 +1,29 @@
+from typing import Literal, Sequence, Tuple
+
 import pandas as pd
-import io
-import requests
 
 
+def separate(
+    dataframe: pd.DataFrame = ..., columns: Sequence[str] = None
+) -> Tuple[pd.DataFrame | pd.Series]:
+    cols = dataframe.columns if not columns else columns
+    return tuple(dataframe[col] for col in cols)
+
+
+def split_large_data(
+    dataframe: pd.DataFrame = ...,
+    n_bins: int = ...,
+    save_as: str = None,
+    file_etx: Literal[".csv", ".txt"] = ".txt",
+) -> None:
+    BINNED = [
+        dataframe.iloc[i : i + n_bins, :] for i in range(0, dataframe.shape[0], n_bins)
+    ]
+
+    if save_as:
+        for i, data in enumerate(BINNED):
+            file = f"{save_as}_{i}.{file_etx}"
+            data.to_csv(file)
+            print(f"Saved '{file}' ({i}/{len(BINNED)})")
+    else:
+        return tuple(BINNED)
--- Fall 2023/DS 440/(002) Midterm Projects/Midterm Project 2 - Final Project/main.py
+++ Fall 2023/DS 440/(002) Midterm Projects/Midterm Project 2 - Final Project/main.py
@@ -1,4 +1,8 @@
+import os
 import pandas as pd
+import helpers as hp
 
 
 DATA = pd.read_csv("DATA\\gaia-dr2-allwise-2mass-ppmxl.csv")
+os.mkdir("DATA\\BINNED_DATA")
+hp.split_large_data(DATA, 8, "DATA\\BINNED_DATA\\gaiaDR2")
