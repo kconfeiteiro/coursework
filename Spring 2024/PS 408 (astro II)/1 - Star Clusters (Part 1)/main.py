67cad3cfc91fa30e40d80849711d898ba5bcfe99
kconfeiteiro <confeitk@outlook.com>
2024-02-03 21:31:08 -0500

    Did work on the PS-408 coding assignment -- DID NOT FINISH.

--- Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/main.py
+++ Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/main.py
@@ -1,9 +1,6 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
 from helpers import list_dir, read_data
-
+from matplotlib import pyplot as plt
 
 CONFIG = {
     "isochrones": {
@@ -14,24 +11,30 @@ CONFIG = {
         "paths": list_dir("DATA\\EVOLUTION_TRACKS"),
         "masses": "0.8 0.9 1 1.1 1.25 1.35 1.5 3 4 7 20 25 32 85 120".split(),
     },
+    "cluster8": {"path": "DATA\\cluster8.txt"},
     "read kwargs": {"delim_whitespace": True, "comment": "#", "on_bad_lines": "skip"},
     "plotted columns": ["logL", "logTe"],
     "sun vals": (3.761, 0),
-
 }
 
-track_data_ZAMS = pd.concat(
-    read_data(
-        CONFIG["tracks"]["paths"],
-        columns=CONFIG["plotted columns"],
-        **CONFIG["read kwargs"],
-    ),
-)
-
-isochrones = read_data(
+TRACK_DATA_ZAMS = read_data(
     CONFIG["tracks"]["paths"],
-    columns=CONFIG["plotted columns"],
     **CONFIG["read kwargs"],
 )
 
-print(isochrones[0])
+ISOCHRONES = read_data(
+    CONFIG["tracks"]["paths"],
+    **CONFIG["read kwargs"],
+)
+
+CLUSTER_8 = pd.read_csv(CONFIG["cluster8"]["path"], **CONFIG["read kwargs"])
+CLUSTER_8["B"] = CLUSTER_8[CLUSTER_8.columns[1]] + CLUSTER_8[CLUSTER_8.columns[2]]
+DATA = (CLUSTER_8["B-V"], CLUSTER_8["B"])
+
+FIG, AXES = plt.subplots()
+AXES.scatter(*DATA, s=5, label="Cluster 8")
+AXES.set_ylabel("Magnitude (B)")
+AXES.set_xlabel("Color Index (B-V)")
+AXES.set_title("CMD - Cluster 8")
+AXES.legend()
+plt.show()
