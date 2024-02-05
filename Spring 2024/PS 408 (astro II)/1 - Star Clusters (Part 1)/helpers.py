86c3da1ef835374e1583d1a8545688c9708ec1dc
kconfeiteiro <confeitk@outlook.com>
2024-02-04 21:09:39 -0500

    Made more progress on the PS-408 coding assignment -- did not finish.

--- Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/helpers.py
+++ Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/helpers.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 def list_dir(rel_path):
@@ -8,6 +9,19 @@ def list_dir(rel_path):
     listed_paths = os.listdir(rel_path)
     prepend = lambda path: os.path.join(ABS, path)
     return list(map(prepend, listed_paths))
+
+
+def uniquefilename(
+    filename,
+    savepath=None,
+):
+    base_name, ext = os.path.splitext(filename)
+    unique_name, counter = filename, 1
+    while os.path.exists(os.path.join(savepath, unique_name)):
+        counter += 1
+        unique_name = f"{base_name}({counter}){ext}"
+
+    return (os.path.join(savepath, unique_name), counter)
 
 
 def read_data(
@@ -40,12 +54,47 @@ def read_data(
     return read_data if len(filenames) > 1 else read_data[0]
 
 
-def fix_data(*dataframes):
-    returns = []
-    for data in dataframes:
-        data.sort_values(data.columns[0], inplace=True)
-        data = data.astype(np.float64)
-        data = (data.iloc[:, 1], data.iloc[:, 0])
-        returns.append(data)
+def read_isochrones(*paths, **kwargs):
+    data = []
+    for path in paths:
+        _temp = pd.read_csv(path, **kwargs)
+        _temp = _temp.iloc[2:, :]
+        _temp = _temp.reset_index()
+        _temp = _temp.drop(columns=["index"])
+        data.append(_temp)
+    return tuple(data)
 
-    return tuple(returns)
+
+def histrogram(
+    data, bins, xtitle, ytitle, figtitle, color="skyblue", edgecolor="black", save_as=None
+):
+    fig, axes = plt.subplots()
+    axes.hist(data=data, bins=bins, color=color, edgecolor=edgecolor)
+    axes.set_xlabel(xtitle)
+    axes.set_ylabel(ytitle)
+    axes.set_title(figtitle)
+
+    if save_as:
+        fig.savefig(save_as)
+
+    plt.show()
+
+
+def prepare_data(*dframes):
+    newdframes = []
+    for dframe in dframes:
+        dframe = dframe[["B-V", "Vmag"]]
+        dframe.sort_values(dframe.columns[0], inplace=True)
+        dframe = dframe.astype(np.float64)
+        dframe = (dframe.iloc[:, 1], dframe.iloc[:, 0])
+        newdframes.append(dframe)
+
+    return tuple(newdframes)
+
+
+def change_col_dtype(*dframes, colname, new_dtype=np.float64, remove_cols=None):
+    for dframe in dframes:
+        if remove_cols:
+            dframe = dframe.drop(columns=remove_cols)
+
+        dframe[colname] = dframe[colname].as_type(new_dtype)
--- Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/main.py
+++ Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from helpers import list_dir, read_data
+from helpers import list_dir, prepare_data, read_isochrones
 from matplotlib import pyplot as plt
 
 CONFIG = {
@@ -12,27 +12,39 @@ CONFIG = {
         "masses": "0.8 0.9 1 1.1 1.25 1.35 1.5 3 4 7 20 25 32 85 120".split(),
     },
     "cluster8": {"path": "DATA\\cluster8.txt"},
-    "read kwargs": {"delim_whitespace": True, "comment": "#", "on_bad_lines": "skip"},
-    "plotted columns": ["logL", "logTe"],
+"read kwargs": {"sep": r"\s+", "comment": "#", "on_bad_lines": "skip"},
+"columns": "B B-V".split(),
     "sun vals": (3.761, 0),
 }
 
-TRACK_DATA_ZAMS = read_data(
-    CONFIG["tracks"]["paths"],
+
+(
+    ISO1,
+    ISO2,
+    ISO3,
+    ISO4,
+    ISO5,
+) = read_isochrones(
+    *CONFIG["isochrones"]["paths"],
     **CONFIG["read kwargs"],
 )
 
-ISOCHRONES = read_data(
-    CONFIG["tracks"]["paths"],
-    **CONFIG["read kwargs"],
-)
 
+# setting up isochrones for plotting
+ISO1, ISO2, ISO3, ISO4, ISO5 = prepare_data(ISO1, ISO2, ISO3, ISO4, ISO5)
+
+# my cluster (cluster #8)
 CLUSTER_8 = pd.read_csv(CONFIG["cluster8"]["path"], **CONFIG["read kwargs"])
-CLUSTER_8["B"] = CLUSTER_8[CLUSTER_8.columns[1]] + CLUSTER_8[CLUSTER_8.columns[2]]
-DATA = (CLUSTER_8["B-V"], CLUSTER_8["B"])
+DATA = (CLUSTER_8["B-V"], CLUSTER_8["V"])
 
+# plot
 FIG, AXES = plt.subplots()
 AXES.scatter(*DATA, s=5, label="Cluster 8")
+AXES.plot(*ISO1, label="ISO 1")
+AXES.plot(*ISO2, label="ISO 2")
+AXES.plot(*ISO3, label="ISO 3")
+AXES.plot(*ISO4, label="ISO 4")
+AXES.plot(*ISO5, label="ISO 5")
 AXES.set_ylabel("Magnitude (B)")
 AXES.set_xlabel("Color Index (B-V)")
 AXES.set_title("CMD - Cluster 8")
