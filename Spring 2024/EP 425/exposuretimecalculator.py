a193d83e1ca99f6c2a1f300654415b4067146ae8
Krystian Confeiteiro <confeitk@my.erau.edu>
2024-02-06 18:30:03 -0500

    Made more progress on the assignment -- DID NOT FINISH.

--- Spring 2024/EP 425/exposuretimecalculator.py
+++ Spring 2024/EP 425/exposuretimecalculator.py
@@ -11,18 +11,18 @@ Created: February, 01 2024
 from math import sqrt
 
 QUADRATICFORM = lambda a, b, c: (-b + sqrt(b**2 - (4 * a * c))) / (2 * a)
-CALC_A = lambda n_star_dot, snr: (n_star_dot / snr) ** 2
-CALC_B = lambda n_star_dot, n_pix, a_b, b_dot, d_dot: -(n_star_dot + (n_pix * a_b(b_dot + d_dot)))
-CALC_C = lambda n_pix, a_b, rho: -(n_pix * a_b * (rho**2))
+CALC_A = lambda N_STAR_DOT, SNR: (N_STAR_DOT / SNR) ** 2
+CALC_B = lambda N_STAR_DOT, N_PIX, A_B, b_dot, d_dot: -(N_STAR_DOT + (N_PIX * A_B(b_dot + d_dot)))
+CALC_C = lambda N_PIX, A_B, RHO: -(N_PIX * A_B * (RHO**2))
 CALC_NSTAR = ...
 
 
 def calc_exposure_time(star_mag: float = ..., snr: float = ...):
     pass
 
-
-
-KWARGS = {
-    "n_star_dot": ...,
-    "": ...
-}
+SNR = ...
+N_STAR_DOT = ...
+N_PIX = ...
+N_PIX = ...
+A_B = ...
+RHO = ...
--- Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/main.py
+++ Spring 2024/PS 408 (astro II)/1 - Star Clusters (Part 1)/main.py
@@ -1,5 +1,22 @@
+"""
+Star Cluster assignment (Part 1) -- PS 408-02DB Spring 2024
+
+Module Information
+------------------
+Workspace: 1 - Star Clusters (Part 1)
+FIlename: main.py
+Path: main.py
+Date: February, 06 2024
+
+Objectives
+------
+1. Plot isochrones from the given ranges
+2. Overlay your cluster's data
+3. Manually adjust the cluster's y-axis data from (2) to match the best isochrone
+"""
+from turtle import distance
 import pandas as pd
-from helpers import list_dir, prepare_data, read_isochrones
+from helpers import list_dir, prepare_data, read_isochrones, distance_modulus
 from matplotlib import pyplot as plt
 
 # assignment configuration dictionary
@@ -10,6 +27,9 @@ CFG = {
     },
     "cluster8": {"path": "DATA\\cluster8.txt"},
     "read kwargs": {"sep": r"\s+", "comment": "#", "on_bad_lines": "skip"},
+    "columns": "B B-V".split(),
+    "sun vals": (3.761, 0),
+    "y-shift": 10.5,
 }
 
 # read data
@@ -29,20 +49,25 @@ ISO1, ISO2, ISO3, ISO4, ISO5 = prepare_data(ISO1, ISO2, ISO3, ISO4, ISO5)
 
 # my cluster (cluster #8)
 CLUSTER_8 = pd.read_csv(CFG["cluster8"]["path"], **CFG["read kwargs"])
-CLUSTER_8["B"] = CLUSTER_8["B-V"] + CLUSTER_8["V"]
-DATA = (CLUSTER_8["B-V"], CLUSTER_8["B"])
+if CFG["y-shift"]:
+    CLUSTER_8["V"] = CLUSTER_8["V"] - CFG["y-shift"]
+
+DATA = (CLUSTER_8["B-V"], CLUSTER_8["V"])
+
+# estimate distance using distance modulus (w/ extinction)
+
 
 # plot
 FIG, AXES = plt.subplots()
 AXES.scatter(*DATA, s=5, label="Cluster 8")
-AXES.plot(*ISO1, label="ISO-1 (6.5)")
-AXES.plot(*ISO2, label="ISO-2 (7.5)")
-AXES.plot(*ISO3, label="ISO-3 (8.5)")
-AXES.plot(*ISO4, label="ISO-4 (9.5)")
-AXES.plot(*ISO5, label="ISO-5 (10.1)")
-AXES.set_ylabel("Magnitude (B)")
-AXES.set_xlabel("Color Index (B-V)")
-AXES.set_title("CMD - Cluster 8")
+AXES.plot(*ISO1[::-1], label="6.5 Myr")
+AXES.plot(*ISO2[::-1], label="7.5 Myr")
+AXES.plot(*ISO3[::-1], label="8.5 Myr")
+AXES.plot(*ISO4[::-1], label="9.5 Myr")
+AXES.plot(*ISO5[::-1], label="10.1 Myr")
+AXES.set_title(r"Color Magnitude Diagram (CMD) - Cluster #8")
+AXES.set_ylabel(r"$B_{mag}$")
+AXES.set_xlabel(r"$B-V$")
 AXES.invert_yaxis()
 AXES.legend()
 plt.show()
